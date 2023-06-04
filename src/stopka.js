93b61a3cb282c39712f984c38e9f2420a84169fa
Wojtek <kozaneckiwojtek@gmail.com>
2023-06-04 18:28:46 +0200

    changes

--- src/stopka.js
+++ src/stopka.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
 import './css/stopka_style.css';
 import social from './img/social.png'
@@ -18,46 +18,46 @@ const Stopka = () => {
     });
   };
 
-    return (
-      <Container fluid className='stopka'>
-  
-  <Row className='wier'>
-    <Col className='kol col-sm-12 col-md-12 col-lg-3'>
-      <h2 class="naglowek_duzy">SZKOŁA LETNIA</h2>
-      <p class="pod_opis">FOLLOW US</p>
-      <img src={social}  alt="social"/>
-    </Col>
-    <Col className='kol col-sm-12 col-md-12 col-lg-3'>
-        <h2 className="naglowki">KONTAKT</h2>
-        <ul className="footer-kontakt">
-              <li id="adres">ul. Szkolna 4, 05-077 Warszawa</li>
-              <li id="tel">+48 123-456-789</li>
-              <li id="mail">szkola.letnia@edu.pl</li>
-        </ul>
-    </Col>
-    <Col className='kol col-sm-12 col-md-12 col-lg-3'>
-        <h2 class="naglowki">LINKI</h2>
-        <ul class="footer-list">
-              <li><Link to="/" onClick={scrollToTop}>Opis</Link></li>
-              <li><Link to="/kursy" onClick={scrollToTop}>Kursy</Link></li>
-              <li><Link to="/form" onClick={scrollToTop}>Formularz rejestracyjny</Link></li>
-        </ul>
-    </Col>
-    <Col className='kol col-sm-12 col-md-12 col-lg-3'>
-      <h2 class="naglowki">MASZ PYTANIA?</h2>
-      <form id="pytnie_form" /*onSubmit={onSubmit} ref={formRef}*/ acceptCharset="UTF-8">
+  return (
+    <Container fluid className='stopka'>
 
-      <input type="text" class="tekst" id="pytanie" placeholder="Wprowadź tekst" />
-      <button type="submit" class="wyslij" onClick={() => window.location.reload()} onclick={scrollToTop()}>Wyślij</button>
-      </form>
-    </Col>
-  </Row>
-  
-  </Container>
-  
-  
+      <Row className='wier'>
+        <Col className='kol col-sm-12 col-md-12 col-lg-3'>
+          <h2 class="naglowek_duzy">SZKOŁA LETNIA</h2>
+          <p class="pod_opis">FOLLOW US</p>
+          <img src={social} alt="social" />
+        </Col>
+        <Col className='kol col-sm-12 col-md-12 col-lg-3'>
+          <h2 className="naglowki">KONTAKT</h2>
+          <ul className="footer-kontakt">
+            <li id="adres">ul. Szkolna 4, 05-077 Warszawa</li>
+            <li id="tel">+48 123-456-789</li>
+            <li id="mail">szkola.letnia@edu.pl</li>
+          </ul>
+        </Col>
+        <Col className='kol col-sm-12 col-md-12 col-lg-3'>
+          <h2 class="naglowki">LINKI</h2>
+          <ul class="footer-list">
+            <li><Link to="/" onClick={scrollToTop}>Opis</Link></li>
+            <li><Link to="/kursy" onClick={scrollToTop}>Kursy</Link></li>
+            <li><Link to="/form" onClick={scrollToTop}>Formularz rejestracyjny</Link></li>
+          </ul>
+        </Col>
+        <Col className='kol col-sm-12 col-md-12 col-lg-3'>
+          <h2 class="naglowki">MASZ PYTANIA?</h2>
+          <form id="pytnie_form" name="pytania" action="/pytania" method="post" /*onSubmit={onSubmit} ref={formRef}*/ acceptCharset="UTF-8">
+            <input type="hidden" name="form-name" value="pytania" />
+            <input type="text" name="pytanie" class="tekst" id="pytanie" placeholder="Wprowadź tekst" />
+            <button type="submit" class="wyslij" onClick={() => window.location.reload()} onclick={scrollToTop()}>Wyślij</button>
+          </form>
+        </Col>
+      </Row>
 
-    );
-  }
-  
-  export default Stopka;
+    </Container>
+
+
+
+  );
+}
+
+export default Stopka;
